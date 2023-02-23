4831ad4a0ab95def2af2b2401f9f126676d21bd3
georgtonramos <georgtonsramos@gmail.com>
2023-02-23 20:30:42 -0300

    Projeto Django - Site Previsão do Tempo

---
+++ weather/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+# Create your views here.
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=pt_br&appid=7b4d52ff2a493573ec428f9db712c28a'
+
+    cities = City.objects.all() #return all the cities in the database
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+
+    form = CityForm()
+
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon'],
+            'humidity' : city_weather['main']['humidity']
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list
+
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/index.html', context) #returns the index.html template
